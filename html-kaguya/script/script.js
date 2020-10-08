2c6b6178830f5cf5ebe5e1053148339ffe673922
RyomaY <docomoknps200uu@gmail.com>
2020-10-08 23:29:31 +0900

    Add: jQueryを用いたハンバーガーメニューの処理を追加

--- html-kaguya/script/script.js
+++ html-kaguya/script/script.js
@@ -3,4 +3,17 @@
 $(function () {
   // 各ページへ遷移したときのアニメーション
   $('main').hide().fadeIn(300);
+
+  // ハンバーガーメニュー, トグルボタン
+  $('.header-hamburger__btn').on('click', function () {
+    $(this).toggleClass('is-active');
+    $('.header-hamburger__menu').toggleClass('is-open');
+  });
+
+  // ハンバーガーメニュー, トグルメニュー
+  $('.header-hamburger__menu').on('click', function () {
+    $(this).toggleClass('is-open');
+    $('.header-hamburger__btn').toggleClass('is-active');
+  });
+
 });
